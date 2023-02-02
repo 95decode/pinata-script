74f5026558992a0b1628d33534a57bfd9e54cc32
95decode <95decode@gmail.com>
2023-02-02 11:43:41 +0900

    Delete test label

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -5,15 +5,9 @@ const levels = {
     info: 1
 }
 
-const nameFormat = winston.format(info => {
-    info.label = "test";
-    return info;
-});
-
 export const logger = winston.createLogger({
     levels: levels,
     format: winston.format.combine(
-        nameFormat(),
         winston.format.timestamp(),
         winston.format.json()
     ),
